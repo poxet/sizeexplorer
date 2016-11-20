07371fc4d5b5bbfd92c194b3029c91b6189e4635
Daniel <daniel.bohlin@gmail.com>
2016-11-20 11:51:18 +0100

    updated chocolatey packages

--- Setup/ChocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = 'windirstat'
-$fileType = 'msi'
-$url = 'http://www.thargelion.net/Resources/#Environment#/SizeExplorer.#Version#.msi'
-$silentArgs = '/q'
-
-Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
